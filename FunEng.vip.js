42879c5a3b0a0f98ebaca8b525b403b57e311627
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-10-25 01:43:58 +0700

    Update FunEng.vip.js

--- FunEng.vip.js
+++ FunEng.vip.js
@@ -3,8 +3,31 @@ var url = $request.url;
 var cons = "getUserInfo";
 if(url.indexOf(cons) != -1)
 {
-obj["data"]["lastVipExpireTime"]= 253378108800;
-obj["data"]["isIapLog"]= true;
+ obj={"data": {
+		"transactionPackages": [{
+			"sku": "com.vocabinnews.sub.30days",
+			"amountOfDays": 90,
+			"lifeTime": true,
+			"flashSale": true
+		}, {
+			"sku": "com.vocabinnews.sub.365days",
+			"amountOfDays": 365,
+			"lifeTime": true,
+			"flashSale": true
+		}, {
+			"sku": "com.vocabinnews.sub.180days",
+			"amountOfDays": 9999991250,
+			"lifeTime": true,
+			"flashSale": true
+		}, {
+			"sku": "com.vocabinnews.sub.lifetime",
+			"amountOfDays": 99991250,
+			"lifeTime": true,
+			"flashSale": true
+		}]
+	},
+	"session": null
+}   
 }
 $done({body: JSON.stringify(obj)});
 
