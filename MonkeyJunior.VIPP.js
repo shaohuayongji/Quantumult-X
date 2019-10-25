1f74657a2a6d6d5e726107fdb69a6d87e5abb057
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-10-25 10:32:07 +0700

    Create MonkeyJunior.VIPP.js

---
+++ MonkeyJunior.VIPP.js
@@ -0,0 +1,238 @@
+{
+	"time_expire": "1582257862",
+	"sticker_free": ["9", "10", "11", "12", "13", "14", "15", "22", "25", "28", "30", "31"],
+	"sticker_paid": ["com.earlystart.xmas.unlock", "com.earlystart.aquarium.unlock", "com.earlystart.atthepark.unlock", "com.earlystart.city1.unlock", "com.earlystart.city2.unlock", "com.earlystart.farmanimal.unlock", "com.earlystart.jungle.unlock", "com.earlystart.jurassic.unlock", "com.earlystart.monsterland.unlock", "com.earlystart.robot.unlock", "com.earlystart.food.unlock", "com.earlystart.zoo.unlock", "com.earlystart.lab.unlock", "com.earlystart.arctic.unlock", "com.earlystart.circus.unlock", "com.earlystart.insects.unlock"],
+	"list_message": [{
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "Thi\u1ebft b\u1ecb hi\u1ec7n t\u1ea1i c\u1ee7a b\u1ea1n \u0111\u00e3 \u0111\u01b0\u1ee3c h\u1ee7y quy\u1ec1n s\u1eed d\u1ee5ng th\u00e0nh c\u00f4ng theo y\u00eau \u0111\u00e3 \u0111\u01b0\u1ee3c g\u1eedi l\u00ean t\u1ed5ng \u0111\u00e0i Monkey Care v\u00e0o 2018\/05\/14 03:15:05",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1526285705",
+		"id": "9",
+		"type": 1
+	}, {
+		"title": "MONKEY STORIES VINH D\u1ef0 \u0110\u1ea0T TOP 1 CH\u01af\u01a0NG TR\u00ccNH GI\u00c1O D\u1ee4C \u0110\u01af\u1ee2C T\u1ea2I V\u1ec0 NHI\u1ec0U NH\u1ea4T T\u1ea0I VI\u1ec6T NAM",
+		"message": "Sau r\u1ea5t nhi\u1ec1u n\u1ed7 l\u1ef1c t\u1eeb \u0111\u1ed9i ng\u0169 ph\u00e1t tri\u1ec3n s\u1ea3n ph\u1ea9m v\u00e0 s\u1ef1 gi\u00fap s\u1ee9c h\u1ebft m\u00ecnh c\u1ee7a c\u00e1c b\u1ed1 m\u1eb9, ch\u01b0\u01a1ng tr\u00ecnh h\u1ecdc Monkey Stories \u0111\u00e3 ch\u00ednh th\u1ee9c \u0111\u1ea1t TOP 1 ch\u01b0\u01a1ng tr\u00ecnh gi\u00e1o d\u1ee5c \u0111\u01b0\u1ee3c t\u1ea3i v\u1ec1 nhi\u1ec1u nh\u1ea5t t\u1ea1i Vi\u1ec7t Nam.\r\nNh\u00e2n s\u1ef1 ki\u1ec7n \u0111\u1eb7c bi\u1ec7t n\u00e0y, Monkey Junior mu\u1ed1n m\u1eddi b\u1ed1 m\u1eb9 c\u00f9ng l\u1eafng nghe nh\u1eefng chia s\u1ebb c\u1ee7a c\u00e1c b\u1ed1 m\u1eb9 \u0111ang cho con s\u1eed d\u1ee5ng ch\u01b0\u01a1ng tr\u00ecnh h\u1ecdc Monkey Stories t\u1ea1i \u0111\u00e2y.",
+		"url": "http:\/\/bit.ly\/Video-Trian",
+		"created": "1528947895",
+		"id": "10",
+		"popup": 0,
+		"type": 1
+	}, {
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "Thi\u1ebft b\u1ecb hi\u1ec7n t\u1ea1i c\u1ee7a b\u1ea1n \u0111\u00e3 \u0111\u01b0\u1ee3c h\u1ee7y quy\u1ec1n s\u1eed d\u1ee5ng th\u00e0nh c\u00f4ng theo y\u00eau \u0111\u00e3 \u0111\u01b0\u1ee3c g\u1eedi l\u00ean t\u1ed5ng \u0111\u00e0i Monkey Care v\u00e0o 2018\/08\/03 03:02:59",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1533283379",
+		"id": "8",
+		"type": 1
+	}, {
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "Thi\u1ebft b\u1ecb hi\u1ec7n t\u1ea1i c\u1ee7a b\u1ea1n \u0111\u00e3 \u0111\u01b0\u1ee3c h\u1ee7y quy\u1ec1n s\u1eed d\u1ee5ng th\u00e0nh c\u00f4ng theo y\u00eau \u0111\u00e3 \u0111\u01b0\u1ee3c g\u1eedi l\u00ean t\u1ed5ng \u0111\u00e0i Monkey Care v\u00e0o 2018\/08\/03 03:50:30",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1533286230",
+		"id": "7",
+		"type": 1
+	}, {
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "MKH c\u1ee7a ba\u0323n (MXJAY) \u0111\u00e3 b\u1ecb g\u1ee1 kh\u1ecfi thi\u1ebft b\u1ecb\u0323 Android - ID 2034592 va\u0300o 2018\/12\/21 09:37:42",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1545359862",
+		"id": "6",
+		"type": 1
+	}, {
+		"title": "G\u00f3i h\u1ecdc h\u1ebft h\u1ea1n",
+		"message": "MKH - 5DXAUYGUA4 \u0111\u00e3 h\u1ebft h\u1ea1n. MKH - 9BVKKGV5BV b\u1eaft \u0111\u1ea7u \u0111\u01b0\u1ee3c s\u1eed d\u1ee5ng. H\u1ea1n s\u1eed d\u1ee5ng: 21\/Dec\/2019",
+		"popup": 1,
+		"url": "",
+		"created": "1545366604",
+		"show_list_device": false,
+		"id": "16",
+		"type": 1
+	}, {
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "MKH c\u1ee7a ba\u0323n (9BVKK-GV5BV) \u0111\u00e3 b\u1ecb g\u1ee1 kh\u1ecfi thi\u1ebft b\u1ecb\u0323 Android - ID 2034592 va\u0300o 2018\/12\/21 11:58:55",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1545368335",
+		"id": "5",
+		"type": 1
+	}, {
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "MKH c\u1ee7a ba\u0323n (75BMP-3Q2TT) \u0111\u00e3 b\u1ecb g\u1ee1 kh\u1ecfi thi\u1ebft b\u1ecb\u0323 Android - ID 2034592 va\u0300o 2018\/12\/21 12:07:39",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1545368859",
+		"id": "4",
+		"type": 1
+	}, {
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "MKH c\u1ee7a ba\u0323n (37MG9-PGNET) \u0111\u00e3 b\u1ecb g\u1ee1 kh\u1ecfi thi\u1ebft b\u1ecb\u0323 Android - ID 2034592 va\u0300o 2018\/12\/21 01:49:17",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1545374957",
+		"id": "3",
+		"type": 1
+	}, {
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "MKH c\u1ee7a ba\u0323n (4UEV3-UDBK7) \u0111\u00e3 b\u1ecb g\u1ee1 kh\u1ecfi thi\u1ebft b\u1ecb\u0323 Android - ID 2034592 va\u0300o 2019\/06\/11 04:26:10",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1560245170",
+		"id": "2",
+		"type": 1
+	}, {
+		"title": "B\u00e9 \u0111\u00e3 ghi nh\u1edb \u0111\u01b0\u1ee3c nhi\u1ec1u t\u1eeb v\u1ef1ng ch\u01b0a?",
+		"message": "C\u00f9ng v\u00e0o ngay ph\u1ea7n h\u1ecdc Ch\u1ee7 \u0111\u1ec1 c\u1ee7a Monkey \u0111\u1ec3 ki\u1ec3m tra l\u1ea1i nh\u00e9!",
+		"url": "mj",
+		"created": "1567647612",
+		"id": "13",
+		"popup": 0,
+		"type": 1
+	}, {
+		"title": "Kho STICKER s\u00e1ng t\u1ea1o \u0111ang ch\u1edd b\u00e9 kh\u00e1m ph\u00e1",
+		"message": "C\u00f9ng v\u00e0o h\u1ecdc Monkey ngay n\u00e0o!",
+		"url": "mj",
+		"created": "1567647617",
+		"id": "12",
+		"popup": 0,
+		"type": 1
+	}, {
+		"title": "\u0110\u00e3 \u0111\u1ebfn gi\u1edd h\u1ecdc Monkey Junior...",
+		"message": "C\u00e1c b\u00e0i h\u1ecdc \u0111ang ch\u1edd b\u00e9 kh\u00e1m ph\u00e1 ngay b\u00e2y gi\u1edd!",
+		"url": "MJ",
+		"created": "1567647632",
+		"id": "11",
+		"popup": 0,
+		"type": 1
+	}, {
+		"title": "Th\u00f4ng b\u00e1o H\u1ee6Y quy\u1ec1n s\u1eed d\u1ee5ng",
+		"message": "MKH c\u1ee7a ba\u0323n (2RPBA-NQMPU) \u0111\u00e3 b\u1ecb g\u1ee1 kh\u1ecfi thi\u1ebft b\u1ecb\u0323 IPad 6 - ID 6153500 va\u0300o 2019\/09\/07 04:34:29",
+		"url": "",
+		"popup": 1,
+		"show_list_device": true,
+		"created": "1567805669",
+		"id": "1",
+		"type": 1
+	}, {
+		"title": "\u0110\u1eebng qu\u00ean t\u1ea1o th\u00f3i quen h\u1ecdc t\u1eadp h\u00e0ng ng\u00e0y cho b\u00e9",
+		"message": "\u0110\u00e3 \u0111\u1ebfn gi\u1edd h\u1ecdc Monkey r\u1ed3i ba m\u1eb9 \u01a1i!",
+		"url": "mj",
+		"created": "1568257870",
+		"id": "15",
+		"popup": 0,
+		"type": 1
+	}, {
+		"title": "Mark v\u00e0 Jane \u0111ang \u0111\u1ee3i b\u00e9 c\u00f9ng kh\u00e1m ph\u00e1 c\u00e1c b\u00e0i h\u1ecdc",
+		"message": "V\u00e0o Monkey ngay n\u00e0o!",
+		"url": "https:\/\/www.monkeyjunior.vn\/",
+		"created": "1569388878",
+		"id": "14",
+		"popup": 0,
+		"type": 1
+	}],
+	"xml_id": "22469465",
+	"url": "",
+	"sort_language": ["1", "12", "4", "21", "7", "9", "8", "10"],
+	"update_mj": [],
+	"current_version": 24,
+	"all_type": 1,
+	"active_download": [],
+	"one_course": 0,
+	"products": [{
+		"phone": "***3615",
+		"email_address": "***in@gmail.com",
+		"total_active": "5 \/ 15",
+		"product_id": "com.earlystart.ltr.1year",
+		"product_name": "G\u00f3i h\u1ecdc s\u1eed d\u1ee5ng 1 n\u0103m",
+		"list_device": {
+			"list_active": [{
+				"device_id": "1737464",
+				"active_date": "1547827414",
+				"device_info": "IPad 5"
+			}, {
+				"device_id": "2034592",
+				"active_date": "1571973437",
+				"device_info": "N\/A"
+			}, {
+				"device_id": "5115537",
+				"active_date": "1547804721",
+				"device_info": "IPhone 6s Plus"
+			}],
+			"list_remove": [{
+				"device_id": "2563174",
+				"active_date": "1545205282",
+				"device_info": "Windows",
+				"time_remove": "1545367110"
+			}, {
+				"device_id": "4671903",
+				"active_date": "1545206420",
+				"device_info": "IPad 3",
+				"time_remove": "1545364489"
+			}]
+		},
+		"time_expire": "1582257862",
+		"status": "\u0110ang s\u1eed d\u1ee5ng"
+	}, {
+		"phone": "***3615",
+		"email_address": "***in@gmail.com",
+		"total_active": "1 \/ 4",
+		"product_id": "com.earlystart.ltr.1year",
+		"product_name": "G\u00f3i h\u1ecdc s\u1eed d\u1ee5ng 1 n\u0103m",
+		"list_device": {
+			"list_active": [{
+				"device_id": "2034592",
+				"active_date": "1545365956",
+				"device_info": "N\/A"
+			}],
+			"list_remove": []
+		},
+		"time_expire": "1545366556",
+		"status": "\u0110ang s\u1eed d\u1ee5ng"
+	}],
+	"device_in_licence": [],
+	"is_vn": "1",
+	"dictPacketTienTe": {
+		"com.earlystart.ltr.1month_sale": "\u0111109,000",
+		"com.earlystart.ltr.1year_sale": "\u0111499,000",
+		"com.earlystart.ltr.2year_sale": "\u0111699,000",
+		"com.earlystart.ltr.4year_sale": "\u01111,199,000",
+		"com.earlystart.ltr.6month_sale": "\u0111409,000"
+	},
+	"dictPrice": {
+		"com.earlystart.ltr.1month_sale": 109000,
+		"com.earlystart.ltr.1year_sale": 499000,
+		"com.earlystart.ltr.2year_sale": 699000,
+		"com.earlystart.ltr.4year_sale": 1199000,
+		"com.earlystart.ltr.6month_sale": 409000
+	},
+	"server": [12, 12],
+	"show_subscribe": "0",
+	"is_delete": 0,
+	"referer_status": "1",
+	"referer_day_active": "12",
+	"install_time": "1568718391",
+	"default_sale30": "1",
+	"sale40_vn": "1",
+	"coming_soon": [],
+	"updateall": 1,
+	"is_licence": true,
+	"show_restore": 0,
+	"show_pay": false,
+	"is_show_ads_app": false,
+	"status": 1,
+	"device_id": 2034592,
+	"multiple_language": 1,
+	"language_display": "21,1,12,8,4,7,9,10",
+	"url_action": "update\/index"
+}
