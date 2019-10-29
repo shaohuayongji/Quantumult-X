75139329be82731585b75f6d2f6412f7c016c968
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-10-29 16:34:15 +0700

    Update FunEng.vip.js

--- FunEng.vip.js
+++ FunEng.vip.js
@@ -3,31 +3,10 @@ var url = $request.url;
 var cons = "getUserInfo";
 if(url.indexOf(cons) != -1)
 {
- obj={"data": {
-		"transactionPackages": [{
-			"sku": "com.vocabinnews.sub.30days",
-			"amountOfDays": 90,
-			"lifeTime": true,
-			"flashSale": true
-		}, {
-			"sku": "com.vocabinnews.sub.365days",
-			"amountOfDays": 365,
-			"lifeTime": true,
-			"flashSale": true
-		}, {
-			"sku": "com.vocabinnews.sub.180days",
-			"amountOfDays": 9999991250,
-			"lifeTime": true,
-			"flashSale": true
-		}, {
-			"sku": "com.vocabinnews.sub.lifetime",
-			"amountOfDays": 99991250,
-			"lifeTime": true,
-			"flashSale": true
-		}]
-	},
-	"session": 758865
-}   
+ obj=["data"]["transactionPackages"]["sku"]="com.vocabinnews.sub.lifetime",
+ obj=["data"]["transactionPackages"]["amountOfDays"]=99991250,
+ obj=["data"]["transactionPackages"]["lifeTime"]=true,	 
+ obj=["data"]["transactionPackages"]["flashSale"]=true,
 }
 $done({body: JSON.stringify(obj)});
 
